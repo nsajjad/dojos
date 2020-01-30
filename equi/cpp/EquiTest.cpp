b4f7ec2120b40cf0515bfc57658591af41d76b32
nsajjad <60460776+nsajjad@users.noreply.github.com>
2020-01-30 08:28:18 +0200

    Update EquiTest.cpp

--- equi/cpp/EquiTest.cpp
+++ equi/cpp/EquiTest.cpp
@@ -2,7 +2,21 @@
 
 int equi(int inputArr[], int arrayLen)
 {
-    // Find equilibrium index of inputArr
+    int i = 0;
+    int sum_left, sum_right = 0;
+    
+    for (i = 0; i < n; i++) {
+        for (int j = 0; j < i; j++) {
+            sum_left += inputArr[j];
+        }
+        for (int j = (i + 1); j < n; j++) {
+            sum_right += inputArr[j];
+        }
+        if (sum_left == sum_right)
+            return i;
+        sum_left = 0;
+        sum_right = 0;
+    }
     return 0;
 }
 
